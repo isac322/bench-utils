aff454672ce125fb82fd436f87336f458b320eae
Byeonghoon Yoo <bh322yoo@gmail.com>
2018-04-20 12:47:50 +0900

    add new parsec benchmarks

--- benchmark/driver/parsec_driver.py
+++ benchmark/driver/parsec_driver.py
@@ -10,7 +10,8 @@ from benchmark.driver.base_driver import BenchDriver
 
 
 class ParsecDriver(BenchDriver):
-    _benches: Set[str] = {'streamcluster', 'canneal', 'swaptions', 'x264', 'ferret', 'bodytrack', 'blackscholes'}
+    _benches: Set[str] = {'streamcluster', 'canneal', 'swaptions', 'x264', 'ferret', 'bodytrack', 'blackscholes',
+                          'dedup', 'facesim', 'fluidanimate', 'freqmine', 'raytrace', 'vips'}
     bench_name: str = 'parsec'
     _bench_home: str = BenchDriver.get_bench_home(bench_name)
 
@@ -19,8 +20,13 @@ class ParsecDriver(BenchDriver):
         return bench_name in ParsecDriver._benches
 
     def _find_bench_proc(self) -> Optional[psutil.Process]:
+        if self._name == 'raytrace':
+            exec_name = 'rtview'
+        else:
+            exec_name = self._name
+
         for process in self._async_proc_info.children(recursive=True):  # type: psutil.Process
-            if process.name() == self._name and process.is_running():
+            if process.name() == exec_name and process.is_running():
                 return process
 
         return None
