e01532f1430ed07fc672cc655b794f4417d54005
Byeonghoon Yoo <bh322yoo@gmail.com>
2018-08-07 20:43:33 +0900

    remove external script dependency from module `benchmark`

--- bench_toolbox/benchmark/drivers/__init__.py
+++ bench_toolbox/benchmark/drivers/__init__.py
@@ -1,31 +1,18 @@
 # coding: UTF-8
 
-import json
 from typing import Optional, Type
 
-from bench_toolbox import GLOBAL_CFG_PATH
 from .base_driver import BenchDriver
 from .npb_driver import NPBDriver
 from .parsec_driver import ParsecDriver
 from .rodinia_driver import RodiniaDriver
 from .spec_driver import SpecDriver
 
-_bench_drivers = (SpecDriver, ParsecDriver, RodiniaDriver, NPBDriver)
-
-
-def _fetch_bench_path():
-    with GLOBAL_CFG_PATH.open() as fp:
-        bench_names = json.load(fp)['benchmark']
-
-    for _bench_driver in _bench_drivers:
-        _bench_driver._bench_home = bench_names[_bench_driver.bench_name]
-
-
-_fetch_bench_path()
+bench_drivers = (SpecDriver, ParsecDriver, RodiniaDriver, NPBDriver)
 
 
 def find_driver(workload_name) -> Type[BenchDriver]:
-    for _bench_driver in _bench_drivers:
+    for _bench_driver in bench_drivers:
         if _bench_driver.has(workload_name):
             return _bench_driver
 
