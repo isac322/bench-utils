abc5b8e246f52b3eae003d3f9fe00a32b30e637d
Yoonsung Nam <ysnam@dcslab.snu.ac.kr>
2018-08-22 12:57:38 +0900

    fix issue#10

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -117,7 +117,7 @@ class Benchmark:
         logger.info(f'The benchmark has started. pid : {self._bench_driver.pid}')
 
         self._pause_bench()
-        bench_path = str(self._bench_driver.name)+'-'+str(self._bench_driver.pid)
+        bench_path = str(self._bench_driver.name)+'_'+str(self._bench_driver.pid)
         self._res_path = self._res_path / bench_path
 
         # create a resource group and register this benchmark to the group
