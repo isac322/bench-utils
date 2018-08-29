59c7ad4640bd2b64c1e63614ad2e3926c26875f6
Byeonghoon Yoo <bh322yoo@gmail.com>
2018-08-29 12:34:50 +0900

    rename ResctrlMonitor to ResCtrlMonitor

--- bench_toolbox/monitors/resctrl_monitor.py
+++ bench_toolbox/monitors/resctrl_monitor.py
@@ -14,7 +14,7 @@ from .handlers.base_handler import BaseHandler
 from .oneshot_monitor import OneShotMonitor
 
 
-class ResctrlMonitor(OneShotMonitor[Tuple[Mapping[str, int], ...]]):
+class ResCtrlMonitor(OneShotMonitor[Tuple[Mapping[str, int], ...]]):
     mount_point = Path('/sys/fs/resctrl')
     mon_features = (mount_point / 'info' / 'L3_MON' / 'mon_features').read_text('ASCII').split()
 
@@ -31,15 +31,15 @@ class ResctrlMonitor(OneShotMonitor[Tuple[Mapping[str, int], ...]]):
         super().__init__(handlers, interval)
 
         self._group_name = grp_name
-        self._group_path = ResctrlMonitor.mount_point / self._group_name
+        self._group_path = ResCtrlMonitor.mount_point / self._group_name
         self._monitoring_pid = parent_pid
 
         # tuple of each feature monitors for each socket
         self._monitors: Tuple[Dict[Path, Optional[AiofilesContextManager]], ...] = tuple(
                 dict.fromkeys(
-                        self._group_path / 'mon_data' / mon.name / feature for feature in ResctrlMonitor.mon_features
+                        self._group_path / 'mon_data' / mon.name / feature for feature in ResCtrlMonitor.mon_features
                 )
-                for mon in ResctrlMonitor._mon_names
+                for mon in ResCtrlMonitor._mon_names
         )
 
     @staticmethod
@@ -58,13 +58,13 @@ class ResctrlMonitor(OneShotMonitor[Tuple[Mapping[str, int], ...]]):
 
             await write_proc.communicate(str(pid).encode())
 
-        if not ResctrlMonitor.mount_point.is_dir():
-            raise PermissionError(f'resctrl is not mounted on {ResctrlMonitor.mount_point.absolute()}')
+        if not ResCtrlMonitor.mount_point.is_dir():
+            raise PermissionError(f'resctrl is not mounted on {ResCtrlMonitor.mount_point.absolute()}')
 
         proc = await asyncio.create_subprocess_exec('sudo', 'mkdir', str(self._group_path), '-p')
         await proc.wait()
 
-        family_pids = ResctrlMonitor._get_all_children(self._monitoring_pid)
+        family_pids = ResCtrlMonitor._get_all_children(self._monitoring_pid)
         await asyncio.wait(tuple(map(write_to_tasks, family_pids)))
 
         async def open_file_async(monitor_dict: Dict[Path, AiofilesContextManager]):
@@ -83,7 +83,7 @@ class ResctrlMonitor(OneShotMonitor[Tuple[Mapping[str, int], ...]]):
         data = tuple(
                 map(dict,
                     await asyncio.gather(*(
-                        asyncio.gather(*map(ResctrlMonitor._read_file, mons.items()))
+                        asyncio.gather(*map(ResCtrlMonitor._read_file, mons.items()))
                         for mons in self._monitors))
                     )
         )
@@ -99,5 +99,6 @@ class ResctrlMonitor(OneShotMonitor[Tuple[Mapping[str, int], ...]]):
         await asyncio.wait(tuple(
                 chain(*(
                     (m.close() for m in mons.values())
-                    for mons in self._monitors))
+                    for mons in self._monitors
+                ))
         ))
