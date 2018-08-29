8f6c6b04190ed3cdcfd01bb2c4c029796a793857
Byeonghoon Yoo <bh322yoo@gmail.com>
2018-08-30 03:17:22 +0900

    redesign resctrl_monitor.py

--- bench_toolbox/monitors/resctrl_monitor.py
+++ bench_toolbox/monitors/resctrl_monitor.py
@@ -4,14 +4,14 @@ import asyncio
 import re
 from itertools import chain
 from pathlib import Path
-from typing import Dict, FrozenSet, Iterable, Mapping, Optional, Tuple
+from typing import Dict, Iterable, Mapping, Optional, Tuple
 
 import aiofiles
 from aiofiles.base import AiofilesContextManager
-from psutil import Process
 
 from .handlers.base_handler import BaseHandler
 from .oneshot_monitor import OneShotMonitor
+from ..benchmark import Benchmark
 
 
 class ResCtrlMonitor(OneShotMonitor[Tuple[Mapping[str, int], ...]]):
@@ -26,39 +26,37 @@ class ResCtrlMonitor(OneShotMonitor[Tuple[Mapping[str, int], ...]]):
             )
     )
 
-    def __init__(self, handlers: Iterable[BaseHandler], interval: int,
-                 grp_name: str = '', parent_pid: int = None) -> None:
+    def __init__(self, handlers: Iterable[BaseHandler], interval: int, bench: Benchmark = None) -> None:
         super().__init__(handlers, interval)
 
-        self._group_name = grp_name
-        self._group_path = ResCtrlMonitor.mount_point / self._group_name
-        self._monitoring_pid = parent_pid
+        self._benchmark: Benchmark = bench
+        self._group_path: Path = ResCtrlMonitor.mount_point
 
         # tuple of each feature monitors for each socket
-        self._monitors: Tuple[Dict[Path, Optional[AiofilesContextManager]], ...] = tuple(
+        self._monitors: Tuple[Dict[Path, Optional[AiofilesContextManager]], ...] = tuple()
+
+    async def _write_to_tasks(self, pid: int) -> None:
+        write_proc: asyncio.subprocess.Process = await asyncio.create_subprocess_exec(
+                'sudo', 'tee', str(self._group_path / 'tasks'),
+                encoding='ASCII',
+                stdin=asyncio.subprocess.PIPE,
+                stdout=asyncio.subprocess.DEVNULL)
+
+        await write_proc.communicate(str(pid).encode())
+
+    async def on_init(self) -> None:
+        await super().on_init()
+
+        if self._benchmark is not None:
+            self._group_path /= self._benchmark.benchmark_name
+
+        # tuple of each feature monitors for each socket
+        self._monitors = tuple(
                 dict.fromkeys(
                         self._group_path / 'mon_data' / mon.name / feature for feature in ResCtrlMonitor.mon_features
                 )
                 for mon in ResCtrlMonitor._mon_names
         )
-
-    @staticmethod
-    def _get_all_children(pid: int) -> FrozenSet[int]:
-        target = Process(pid)
-        all_children = ((t.id for t in proc.threads()) for proc in target.children(recursive=True))
-        return frozenset(chain(*all_children, map(lambda t: t.id, target.threads())))
-
-    async def on_init(self) -> None:
-        await super().on_init()
-
-        async def write_to_tasks(pid: int) -> None:
-            write_proc: asyncio.subprocess.Process = await asyncio.create_subprocess_exec(
-                    'sudo', 'tee', str(self._group_path / 'tasks'),
-                    encoding='ASCII',
-                    stdin=asyncio.subprocess.PIPE,
-                    stdout=asyncio.subprocess.DEVNULL)
-
-            await write_proc.communicate(str(pid).encode())
 
         if not ResCtrlMonitor.mount_point.is_dir():
             raise PermissionError(f'resctrl is not mounted on {ResCtrlMonitor.mount_point.absolute()}')
@@ -66,8 +64,8 @@ class ResCtrlMonitor(OneShotMonitor[Tuple[Mapping[str, int], ...]]):
         proc = await asyncio.create_subprocess_exec('sudo', 'mkdir', str(self._group_path), '-p')
         await proc.wait()
 
-        family_pids = ResCtrlMonitor._get_all_children(self._monitoring_pid)
-        await asyncio.wait(tuple(map(write_to_tasks, family_pids)))
+        if self._benchmark is not None:
+            await asyncio.wait(tuple(map(self._write_to_tasks, self._benchmark.all_child_tid())))
 
         async def open_file_async(monitor_dict: Dict[Path, AiofilesContextManager]):
             for file_path in monitor_dict:
