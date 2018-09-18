72a9394a818c2f59223ff24ead7f101f89485d51
Byeonghoon Yoo <bh322yoo@gmail.com>
2018-09-18 14:57:18 +0900

    remove redundant code from utils/cgroup/cpu.py

--- bench_toolbox/utils/cgroup/cpu.py
+++ bench_toolbox/utils/cgroup/cpu.py
@@ -3,8 +3,6 @@
 import asyncio
 from typing import ClassVar
 
-import aiofiles
-
 from .base import BaseCGroup
 
 
@@ -18,19 +16,3 @@ class CPU(BaseCGroup):
     async def assign_period(self, period: int) -> None:
         proc = await asyncio.create_subprocess_exec('cgset', '-r', f'cpu.cfs_period_us={period}', self._name)
         await proc.communicate()
-
-    # FIXME: remove
-    async def limit_cpu_quota(self, limit_percentage: float, period: int = None) -> None:
-        if period is None:
-            async with aiofiles.open(str(self.absolute_path() / 'cpu.cfs_period_us')) as afp:
-                line: str = await afp.readline()
-                period = int(line)
-
-        cpu_cores = await self._get_cpu_affinity_from_group()
-        quota = int(period * limit_percentage / 100 * len(cpu_cores))
-
-        quota_proc = await asyncio.create_subprocess_exec('cgset', '-r', f'cpu.cfs_quota_us={quota}', self._name)
-        await quota_proc.communicate()
-
-        period_proc = await asyncio.create_subprocess_exec('cgset', '-r', f'cpu.cfs_period_us={period}', self._name)
-        await period_proc.communicate()
