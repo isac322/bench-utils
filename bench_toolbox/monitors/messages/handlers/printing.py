8c1f4decc9bc76109c8df3164244d1934cfca435
Byeonghoon Yoo <bh322yoo@gmail.com>
2018-11-11 00:18:57 +0900

    add docstring to bench_tools/monitors/messages/handlers/printing.py

--- bench_toolbox/monitors/messages/handlers/printing.py
+++ bench_toolbox/monitors/messages/handlers/printing.py
@@ -11,6 +11,14 @@ if TYPE_CHECKING:
 
 
 class PrintHandler(BaseHandler):
+    """
+    전달되는 모든 메시지를 ``stdout``으로 출력하고, 메시지를 그대로 다음 핸들러에게 전달하는 핸들러.
+
+    .. note::
+
+        디버깅용이기 때문에, 로깅에 사용하지 않는다.
+    """
+
     async def on_message(self, message: MonitoredMessage) -> Optional[MonitoredMessage]:
         print({'data': message.data, 'src': message.source.__class__.__name__})
         return message
