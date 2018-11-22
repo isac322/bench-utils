26884da6cd5332f48e0b490254243996518149c2
Byeonghoon Yoo <bh322yoo@gmail.com>
2018-11-23 01:28:11 +0900

    add docstring to monitors.messages module

--- bench_toolbox/monitors/messages/__init__.py
+++ bench_toolbox/monitors/messages/__init__.py
@@ -1,5 +1,26 @@
 # coding: UTF-8
 
+"""
+:mod:`messages` -- 파이프라인에 전달되는 메시지
+=======================================================
+
+각종 :class:`모니터 <bench_toolbox.monitors.base.BaseMonitor>` 으로부터 최초 생성되어지며,
+:class:`파이프라인 <bench_toolbox.monitors.pipelines.base.BasePipeline>` 로 전달되어 해당 파이프라인에 바인딩된
+:class:`핸들러 <bench_toolbox.monitors.messages.handlers.base.BaseHandler>` 들에게 차례대로 전달되어 처리된다.
+
+기본적으로 read-only 객체이며, 모니터로부터 생성되거나, 메시지 핸들러로부터 생성될 수 있다.
+
+.. seealso::
+
+    :mod:`bench_toolbox.monitors.pipelines` 모듈
+        메시지가 전송되어 처리되는 파이프라인.
+        메시지가 사용되는 방식과 재생산되는 방식이 설명 됨.
+
+.. module:: bench_toolbox.monitors.messages
+    :synopsis: 모니터로부터 생성돼 파이프라인에 전달되는 메시지
+.. moduleauthor:: Byeonghoon Yoo <bh322yoo@gmail.com>
+"""
+
 from .base import BaseMessage, GeneratedMessage, MonitoredMessage
 from .per_bench import PerBenchMessage
 from .rabbit_mq import RabbitMQMessage
--- bench_toolbox/monitors/messages/base.py
+++ bench_toolbox/monitors/messages/base.py
@@ -16,14 +16,32 @@ T = TypeVar('T')
 
 @dataclass(frozen=True)
 class BaseMessage(Generic[T], metaclass=ABCMeta):
+    """
+    모니터 혹은 핸들러에서 생성되어 파이프라인을 통해 핸들러들에게 전달되어지는 메시지.
+
+    .. seealso::
+
+        :mod:`bench_toolbox.monitors` 모듈
+            주로 메시지를 만들어내는 모니터
+
+        :mod:`bench_toolbox.monitors.pipelines` 모듈
+            메시지가 흐르는 파이프라인
+
+        :mod:`bench_toolbox.monitors.messages.handlers` 모듈
+            메시지를 처리하거나 또 다른 메시지를 만들기로 하는 메시지 핸들러
+    """
     data: T
 
 
 @dataclass(frozen=True)
 class MonitoredMessage(BaseMessage[T], metaclass=ABCMeta):
+    """ :class:`모니터 <bench_toolbox.monitors.base.BaseMonitor>` 를 통해 생성된 메시지 """
     source: BaseMonitor[T]
+    """ 본 메시지를 생성한 모니터 객체 """
 
 
 @dataclass(frozen=True)
 class GeneratedMessage(BaseMessage[T], metaclass=ABCMeta):
+    """ 모니터가 아닌 :class:`메시지 핸들러 <bench_toolbox.monitors.messages.base.BaseHandler>` 를 통해 생성된 메시지 """
     generator: BaseHandler
+    """ 본 메시지를 생성한 핸들러 객체 """
