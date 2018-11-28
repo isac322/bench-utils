d84f824f3bfb788cc2bf9c40e85d4f0f394f2941
Byeonghoon Yoo <bh322yoo@gmail.com>
2018-11-29 00:50:41 +0900

    add docstring to monitors.pipelines

--- bench_toolbox/monitors/pipelines/default.py
+++ bench_toolbox/monitors/pipelines/default.py
@@ -4,4 +4,8 @@ from .base import BasePipeline
 
 
 class DefaultPipeline(BasePipeline):
+    """
+    프로토타입으로 멀티 스레드도 아니며, 메시지간의 순서도 지키지 않으며 동작하는 파이프라인.
+    추후 이 클래스를 지우고, 각각의 특징이 있는 다른 파이프라인을 개발이 필요함.
+    """
     pass
