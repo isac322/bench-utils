c3b00aa696675c67e2a667df76a8a0754c2e56ca
Byeonghoon Yoo <bh322yoo@gmail.com>
2018-11-29 00:53:53 +0900

    fix module description of monitors.messages, monitors.messages.handlers

--- bench_toolbox/monitors/messages/__init__.py
+++ bench_toolbox/monitors/messages/__init__.py
@@ -4,11 +4,13 @@
 :mod:`messages` -- 파이프라인에 전달되는 메시지
 =======================================================
 
-각종 :class:`모니터 <bench_toolbox.monitors.base.BaseMonitor>` 으로부터 최초 생성되어지며,
+각종 :class:`모니터 <bench_toolbox.monitors.base.BaseMonitor>` 로부터 최초 생성되어지며,
 :class:`파이프라인 <bench_toolbox.monitors.pipelines.base.BasePipeline>` 로 전달되어 해당 파이프라인에 바인딩된
 :class:`핸들러 <bench_toolbox.monitors.messages.handlers.base.BaseHandler>` 들에게 차례대로 전달되어 처리된다.
 
-기본적으로 read-only 객체이며, 모니터로부터 생성되거나, 메시지 핸들러로부터 생성될 수 있다.
+기본적으로 read-only 컨테이너 객체이며, 모니터로부터 생성되거나, 메시지 핸들러로부터 생성될 수 있다.
+
+메시지의 내용은 타입마다 다르지만, 보통 모니터링으로 관찰된 데이터를 내용으로 가진다.
 
 .. seealso::
 
--- bench_toolbox/monitors/messages/handlers/__init__.py
+++ bench_toolbox/monitors/messages/handlers/__init__.py
@@ -5,15 +5,18 @@
 =======================================================
 
 :class:`파이프라인 <bench_toolbox.monitors.pipelines.base.BasePipeline>` 에 등록되어서 해당 파이프라인에 메시지가 전달될 때
-등록된 핸들러들이 순차적으로 메시지를 처리해나간다.
+등록된 핸들러들이 순차적으로 :class:`메시지 <bench_toolbox.monitors.messages.base.BaseMessage>` 를 처리해나간다.
 
+핸들러는 파이프라인에 등록되어 자신에게 주어지는 메시지를 처리하고, 다음 핸들러에게 자신이 받은 메시지를 그대로 전달하거나,
+새로운 메시지를 생성하여 보내는 일을 한다.
 
-각 핸들러는 독립적이라서 서로에게 영향을 줄 수 있는 방법은, 파이프라이닝하며 전달되는
-:class:`메시지 객체 <bench_toolbox.monitors.messages.base.BaseMessage>` 뿐이다.
+같은 파이프라인에 등록된 핸들러라고 하더라도, 각 핸들러는 독립적이기 때문에 핸들러끼리 직접적으로 데이터를 주고받는 방법은 존재하지않다.
+
+핸들러는 구현에따라 모든 메시지를 처리하거나, 특정 클래스의 객체만 처리하도록 구현할 수 있다.
 
 .. seealso::
 
-    Module :mod:`bench_toolbox.monitors.pipelines`
+    :mod:`bench_toolbox.monitors.pipelines` 모듈
         핸들러가 바인딩 되는 파이프라인. 핸들러가 사용되는 방식이 설명 됨.
 
 .. module:: bench_toolbox.monitors.messages.handlers
