1531a8d163a7d1eb0c2f16df4310a340f56e651e
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-02-22 15:14:13 +0900

    make config containers ContextReadable

--- benchmon/configs/containers/bench.py
+++ benchmon/configs/containers/bench.py
@@ -6,24 +6,36 @@ import logging
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
-from typing import TYPE_CHECKING, Tuple
+from typing import Optional, TYPE_CHECKING, Tuple
 
 from .base import BaseConfig
-from ...benchmark import LaunchableBenchmark
+from ... import ContextReadable
+from ...benchmark import BaseBenchmark, LaunchableBenchmark
 
 if TYPE_CHECKING:
+    from ... import Context
     from ...benchmark.base_builder import BaseBuilder as BenchmarkBuilder
     from ...benchmark.constraints import BaseConstraint
 
 
 @dataclass(frozen=True)
-class BenchConfig(BaseConfig, metaclass=ABCMeta):
+class BenchConfig(BaseConfig, ContextReadable, metaclass=ABCMeta):
     num_of_threads: int
     type: str
     constraints: Tuple[BaseConstraint, ...]
     identifier: str
     workspace: Path
     width_in_log: int
+
+    @classmethod
+    def of(cls, context: Context) -> Optional[BenchConfig]:
+        benchmark = BaseBenchmark.of(context)
+
+        if benchmark is None:
+            return None
+        else:
+            # noinspection PyProtectedMember
+            return benchmark._bench_config
 
     @abstractmethod
     def generate_builder(self, logger_level: int = logging.INFO) -> BenchmarkBuilder:
@@ -33,6 +45,10 @@ class BenchConfig(BaseConfig, metaclass=ABCMeta):
 @dataclass(frozen=True)
 class LaunchableConfig(BenchConfig):
     name: str
+
+    @classmethod
+    def of(cls, context: Context) -> Optional[LaunchableConfig]:
+        return super().of(context)
 
     def generate_builder(self, logger_level: int = logging.INFO) -> LaunchableBenchmark.Builder:
         return LaunchableBenchmark.Builder(self, logger_level)
--- benchmon/configs/containers/perf.py
+++ benchmon/configs/containers/perf.py
@@ -1,9 +1,17 @@
 # coding: UTF-8
 
+from __future__ import annotations
+
 from dataclasses import dataclass
-from typing import Any, Generator, Tuple
+from typing import Any, Generator, Optional, TYPE_CHECKING, Tuple
 
 from .base import MonitorConfig
+from ... import ContextReadable
+from ...benchmark import BaseBenchmark
+from ...monitors import PerfMonitor
+
+if TYPE_CHECKING:
+    from ... import Context
 
 
 @dataclass(frozen=True)
@@ -13,9 +21,24 @@ class PerfEvent:
 
 
 @dataclass(frozen=True)
-class PerfConfig(MonitorConfig):
+class PerfConfig(MonitorConfig, ContextReadable):
     interval: int
     events: Tuple[PerfEvent, ...]
+
+    @classmethod
+    def of(cls, context: Context) -> Optional[PerfConfig]:
+        benchmark = BaseBenchmark.of(context)
+
+        if benchmark is None:
+            return None
+
+        # noinspection PyProtectedMember
+        for monitor in benchmark._monitors:
+            if isinstance(monitor, PerfMonitor):
+                # noinspection PyProtectedMember
+                return monitor._perf_config
+
+        return None
 
     @property
     def event_names(self) -> Generator[str, Any, None]:
