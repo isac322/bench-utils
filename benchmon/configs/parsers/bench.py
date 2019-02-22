e886351718125dd46aad472db3a10b4254ffd9b9
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-02-22 15:26:09 +0900

    refactor configs/parsers/bench.py

--- benchmon/configs/parsers/bench.py
+++ benchmon/configs/parsers/bench.py
@@ -3,29 +3,29 @@
 from __future__ import annotations
 
 from itertools import chain, groupby
-from typing import Dict, Iterable, List, Optional, Tuple
+from typing import Iterable, List, Optional
 
 from .base import LocalReadParser
 from .benchmark.base import BaseBenchParser, BenchJson
 from ..containers import BenchConfig
 
 
+# FIXME: rename to ExperimentParser
 class BenchParser(LocalReadParser[Iterable[BenchConfig]]):
     def _parse(self) -> Iterable[BenchConfig]:
         configs: List[BenchJson] = self._local_config['workloads']
         default_type: Optional[str] = self._local_config.get('default_wl_parser')
 
-        configs.sort(key=(lambda x: x.get('parser', default_type)))
-        cfg_dict: Dict[str, Tuple[BenchJson, ...]] = {
-            bench_type: tuple(cfg_list)
-            for bench_type, cfg_list in groupby(configs, lambda x: x.get('parser', default_type))
-        }
+        def _config_key(config: BenchJson):
+            return config.get('parser', default_type)
+
+        configs.sort(key=_config_key)
 
         return chain(
                 *(
                     BaseBenchParser
                         .get_parser(bench_type)
-                        .parse(cfg_list, self._workspace)
-                    for bench_type, cfg_list in cfg_dict.items()
+                        .parse(tuple(cfg_list), self._workspace)
+                    for bench_type, cfg_list in groupby(configs, _config_key)
                 )
         )
