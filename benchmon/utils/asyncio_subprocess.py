346372b7dc3e0fd9bacca04bc96097b5bde262e9
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-03-10 02:20:46 +0900

    fixes duplicated stderr key error

--- benchmon/utils/asyncio_subprocess.py
+++ benchmon/utils/asyncio_subprocess.py
@@ -2,18 +2,18 @@
 
 import asyncio
 from subprocess import CalledProcessError
+from typing import Union
 
 
 # noinspection PyShadowingBuiltins
-async def check_output(program: str, *args, input: bytes = None, **kwargs) -> str:
+async def check_output(program: str, *args, input: bytes = None,
+                       stderr: int = asyncio.subprocess.PIPE, **kwargs) -> Union[bytes, str]:
     if input is not None:
         if 'stdin' in kwargs:
             raise ValueError('stdin and input arguments may not both be used.')
         kwargs['stdin'] = asyncio.subprocess.PIPE
 
-    proc = await asyncio.create_subprocess_exec(program, *args,
-                                                stdout=asyncio.subprocess.PIPE,
-                                                stderr=asyncio.subprocess.PIPE, **kwargs)
+    proc = await asyncio.create_subprocess_exec(program, stdout=asyncio.subprocess.PIPE, stderr=stderr, *args, **kwargs)
     out, err = await proc.communicate(input)
 
     if proc.returncode:
