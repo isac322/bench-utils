0389ce60fa379165fb0ea68f66eb3554d22db0d9
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-03-15 02:12:29 +0900

    fixes post_scripts/validate_perf.py

--- hybrid_iso/post_scripts/validate_perf.py
+++ hybrid_iso/post_scripts/validate_perf.py
@@ -3,20 +3,23 @@
 
 import sys
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
+from benchmon.configs.containers import BenchConfig
+from benchmon.configs.parsers import BenchParser
 from .tools import WorkloadResult, read_result
 
 
 def run(workspace: Path, global_cfg_path: Path):
-    results: List[WorkloadResult] = read_result(workspace)
+    bench_configs: Tuple[BenchConfig, ...] = tuple(BenchParser(workspace).parse())
+    results: List[WorkloadResult] = read_result(bench_configs)
 
     error_file = workspace / 'ERROR'
     if error_file.exists():
         error_file.unlink()
 
     for result in results:
-        if 0 in result.metrics.get('llc_occupancy'):
+        if 0 in result.perf.get('llc_occupancy'):
             print(f'{result.name} has 0 llc_occupancy value!', file=sys.stderr)
 
             if not error_file.exists():
