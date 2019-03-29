f69ce6e4c86905e2230eac57fb058a47f6794ad1
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-03-30 03:38:46 +0900

    modified drop_privilege to change both of ruid and euid.

--- benchmon/utils/privilege.py
+++ benchmon/utils/privilege.py
@@ -6,17 +6,19 @@ import os
 
 @contextlib.contextmanager
 def drop_privilege(target_uid: int, target_gid: int):
-    should_modify_gid = target_gid is not 0 and target_gid != os.getegid()
-    should_modify_uid = target_uid is not 0 and target_uid != os.geteuid()
+    backup_egid = os.getegid()
+    should_modify_gid = target_gid != backup_egid
+    backup_euid = os.geteuid()
+    should_modify_uid = target_uid != backup_euid
 
     if should_modify_gid:
-        os.setegid(target_gid)
+        os.setresgid(target_gid, target_gid, backup_egid)
     if should_modify_uid:
-        os.seteuid(target_uid)
+        os.setresuid(target_uid, target_uid, backup_euid)
 
     yield
 
     if should_modify_gid:
-        os.setegid(0)
+        os.setresgid(backup_egid, backup_egid, backup_egid)
     if should_modify_uid:
-        os.seteuid(0)
+        os.setresuid(backup_euid, backup_euid, backup_euid)
